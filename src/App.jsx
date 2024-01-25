11ae4ff663e1bfccc6138287b24ed37f5e797687
beth-ina <134806918+beth-ina@users.noreply.github.com>
2024-01-25 01:35:35 -0800

    changes to start from top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
+
 import Home from "./components/home/Home";
-import { Routes, Route } from "react-router-dom";
 import Shared from "./components/sharedlayout/Shared";
 import AboutDetail from "./components/about/AboutDetail";
 import Login from "./components/login/Login";
@@ -10,13 +12,23 @@ import IndustrialDetail from "./components/IndustrialDetail/IndustrialDetail";
 import GemstoneList from "./components/GemstonList/GemstonList";
 import IndustrialList from "./components/IndutrialList/IndustrialList";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Shared />}>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/gemstone/:id" element={<GemstoneDetail />} />
           <Route path="/industrial/:id" element={<IndustrialDetail />} />
           <Route path="/about" element={<AboutDetail />} />
